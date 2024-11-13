0490b45214baccf7c4114d1782a2fbc025d8214a
subin <cdw3521@naver.com>
2024-11-13 15:45:27 +0900

    🔧 환경변수 설정 추가, 파일 및 이름 변경

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TestController } from './keyword/keyword.controller';
-import { TestService } from './keyword/keyword.service';
+import { KeywordController } from './keyword/keyword.controller';
+import { KeywordService } from './keyword/keyword.service';
 import { MySqlService } from './my-sql/my-sql.service';
+import { RegionController } from './region/region.controller';
+import { RegionService } from './region/region.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController, TestController],
-  providers: [AppService, TestService, MySqlService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+  ],
+  controllers: [AppController, KeywordController, RegionController],
+  providers: [AppService, RegionService, MySqlService, KeywordService],
 })
 export class AppModule {}
