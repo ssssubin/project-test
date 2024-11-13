8fd5a7f614a71fec3d0abf3fcc11967bc6dc5efb
subin <cdw3521@naver.com>
2024-11-13 15:46:52 +0900

    🗃️ 데이터 조회하는 쿼리 추가 - 키워드 id로 키워드 이름 조회 - 도시 id로 여행지 조회 - 도시 이름으로 도시 id 조회 - 여행지 id로 키워드 id 조회

--- src/my-sql/my-sql.service.ts
+++ src/my-sql/my-sql.service.ts
@@ -12,20 +12,34 @@ export class MySqlService {
       port: 3306,
       user: process.env.USER,
       password: process.env.PASSWORD,
-      database: 'test1',
+      database: 'test',
     });
   }
 
-  // 키워드 id 찾는 쿼리
+  // 키워드 이름으로 키워드 id 찾는 쿼리
   async findKeywordId(keyword: string) {
     const sql = `SELECT id FROM keyword WHERE name = "${keyword}"`;
     const [rows] = await this.pool.execute(sql);
     return rows;
   }
 
-  // 여행지 찾는 쿼리
+  // 키워드 id로 키워드 이름 찾는 쿼리
+  async findKeywordNameById(id: number) {
+    const sql = `SELECT name FROM keyword WHERE id = "${id}"`;
+    const [rows] = await this.pool.execute(sql);
+    return rows;
+  }
+
+  // 여행지 id로 여행지 찾는 쿼리
   async findDestination(id: number) {
     const sql = `SELECT name, address, city_id FROM destination WHERE id = "${id}"`;
+    const [rows] = await this.pool.execute(sql);
+    return rows;
+  }
+
+  // 도시 id로 여행지 찾는 쿼리
+  async findDestinationByCity(id: number) {
+    const sql = `SELECT id, name, address FROM destination WHERE city_id = "${id}" `;
     const [rows] = await this.pool.execute(sql);
     return rows;
   }
@@ -33,6 +47,20 @@ export class MySqlService {
   // 리전 조회하는 쿼리
   async getRegion(id: number) {
     const sql = `SELECT co.name as country, ci.name as city FROM cities as ci JOIN countries as co ON ci.country_id = co.id and ci.country_id = "${id}"`;
+    const [rows] = await this.pool.execute(sql);
+    return rows;
+  }
+
+  // 도시 이름으로 도시 id 찾는 쿼리
+  async findCityId(name: string) {
+    const sql = `SELECT id FROM cities WHERE name = "${name}"`;
+    const [rows] = await this.pool.execute(sql);
+    return rows;
+  }
+
+  // 여행지 id로 키워드 id 찾는 쿼리
+  async findKeywordIdByDestinationId(id: number) {
+    const sql = `SELECT keyword_id FROM destination_keyword WHERE destination_id = "${id}"`;
     const [rows] = await this.pool.execute(sql);
     return rows;
   }
