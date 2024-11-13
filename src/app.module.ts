29e8c77d21bb73ab0732e3fc3a0daafa2c6ebb5f
subin <cdw3521@naver.com>
2024-11-13 11:20:10 +0900

    🚚 폴더명 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TestController } from './test/test.controller';
-import { TestService } from './test/test.service';
+import { TestController } from './keyword/keyword.controller';
+import { TestService } from './keyword/keyword.service';
 import { MySqlService } from './my-sql/my-sql.service';
 
 @Module({
